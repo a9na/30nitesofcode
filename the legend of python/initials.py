debfb68af650374308c3eadfd2d1d8d1bc231e06
Ana Novković <anaiscoding@gmail.com>
2024-11-26 14:02:26 +0100

    Create initials.py

---
+++ the legend of python/initials.py
@@ -0,0 +1,16 @@
+# Write code below 💖
+
+# Printing out a message 
+print('Hi')
+
+print('Hi')   # I'm a comment, too!
+
+# Hi! I am Ana and I really like dancing !
+
+print('  A   N   N')
+print(' A A  NN  N')
+print('A   A N N N')
+print('AAAAA N  NN')
+print('A   A N   N')
+print('A   A N   N')
+print('A   A N   N')
